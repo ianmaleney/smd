7106ef224c7cda7ff33c1a8a7a0fa9022f242965
Ian Maleney <imaleney@gmail.com>
2018-08-17 22:23:32 +0100

    heroku-test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -12,6 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          Heroku test.
         </p>
       </div>
     );
